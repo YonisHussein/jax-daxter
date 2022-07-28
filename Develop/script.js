daaf2b4baecf2d9353779afeac7821118f46f63e
YonisHussein <yonismhussein@gmail.com>
2022-07-27 21:59:39 -0400

    continuing the if else statement

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var password;
-var number;
-var upperCaseLetter;
-var lowerCaseLetter;
-var specialCharacter;
+var passwordConfirm;
+var numberConfirm;
+var upperCaseLetterConfirm;
+var lowerCaseLetterConfirm;
+var specialCharacterConfirm;
 var userChoice;
 
 var lowerCaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"];
@@ -13,6 +13,7 @@ var blankUpper = [];
 var toUpper = function (x) {
   return x.toUpperCase();
 };
+upperCaseLetter = lowerCaseLetter.map(toUpper);
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
@@ -37,17 +38,48 @@ function generatePassword() {
     console.log("Password length " + password);
 
   } else {
-    lowerCaseLetter = confirm("Do you want lowercase letters?");
+    lowerCaseLetterConfirm = confirm("Do you want lowercase letters?");
     console.log("Lower case " + lowerCaseLetter);
-    upperCaseLetter = confirm("Do you want uppercase letters?");
+    upperCaseLetterConfirm = confirm("Do you want uppercase letters?");
     console.log("Upper case " + upperCaseLetter);
-    number = confirm("Do you want numbers?");
+    numberConfirm = confirm("Do you want numbers?");
     console.log("Number " + number);
-    specialCharacter = confirm("Do you want special characters");
+    specialCharacterConfirm = confirm("Do you want special characters");
     console.log("Special Character " + specialCharacter);
   };
 
-  if(!lowerCaseLetter && !upperCaseLetter && !number && !specialCharacter) {
-    userChoice
+  if(!lowerCaseLetterConfirm && !upperCaseLetterConfirm && !numberConfirm && !specialCharacterConfirm) {
+    userChoice = alert("");
+
+  } 
+  
+  else if (lowerCaseLetterConfirm && upperCaseLetterConfirm && numberConfirm && specialCharacterConfirm) {
+    userChoice = lowerCaseLetter.concat(upperCaseLetter, number, specialCharacter);
+    console.log(userChoice);
+  }
+
+  else if (lowerCaseLetterConfirm && upperCaseLetterConfirm && specialCharacter) {
+    userChoice = lowerCaseLetter.concat(upperCaseLetter, specialCharacter);
+    console.log(userChoice);
+  }
+
+  else if (lowerCaseLetterConfirm && upperCaseLetterConfirm && numberConfirm) {
+    userChoice = lowerCaseLetter.concat(upperCaseLetter, number);
+    console.log(userChoice);
+  }
+
+  else if (upperCaseLetterConfirm && numberConfirm && specialCharacter) {
+    userChoice = lowerCaseLetter.concat(number, specialCharacter);
+    console.log(userChoice);
+  }
+
+  else if (upperCaseLetterConfirm && numberConfirm && specialCharacter) {
+    userChoice = upperCaseLetter.concat(number, specialCharacter);
+    console.log(userChoice);
+  }
+
+  else if (lowerCaseLetterConfirm && upperCaseLetterConfirm) {
+    userChoice = lowerCaseLetter.concat(upperCaseLetter);
+    console.log(userChoice);
   }
 }
